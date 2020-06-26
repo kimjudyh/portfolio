93b537223ac40a7eb65f00e71d56eb2dd7f374b3
Judy <kimjudyh@gmail.com>
2020-06-26 15:12:12 -0700

    started project template component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import ProjectTemplate from './ProjectTemplate';
 
 const Project = () => {
   return (
     <div className="project">
-      <h2>project description</h2>
+      <h2>projects</h2>
       this is project owo
-      <h2>project technology</h2>
-      <h2>best parts of project</h2>
-      <h2>what I'm most proud of</h2>
+      <ProjectTemplate />
     </div>
   )
 }
---
+++ src/components/ProjectTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectTemplate = (props) => {
+  return (
+    <>
+      <h3>project technology</h3>
+      <h3>best parts of project</h3>
+      <h3>what I'm most proud of</h3>
+      this will be filled with stuff
+    </>
+  )
+}
+
+export default ProjectTemplate;
