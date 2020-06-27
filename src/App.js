d8d04f162a6367fa306142751ac5abaffdfe41a2
Judy <kimjudyh@gmail.com>
2020-06-26 17:54:24 -0700

    put app content into container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Project from './components/Project';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       </h1>
       <About />
       <Project />
+      <Resume />
     </div>
   );
 }
