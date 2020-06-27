acbebab8346dc4438deac1a542ec4c211a8baed2
Judy <kimjudyh@gmail.com>
2020-06-26 17:55:10 -0700

    added project thumbnails, links

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,8 +5,51 @@ const Project = () => {
   return (
     <div className="project">
       <h2>projects</h2>
-      this is project owo
-      <ProjectTemplate />
+      <p>
+        this is project owo
+      </p>
+      {/* Project 1 - Pet a Bunny */}
+      <ProjectTemplate
+        thumbnailURL={process.env.PUBLIC_URL + '/images/game_preview.png'}
+        projectURL='https://pages.git.generalassemb.ly/judykim-ga/pet-a-bunny/'
+        githubURL='https://git.generalassemb.ly/judykim-ga/pet-a-bunny'
+      />
+
+      {/* Project 2 - Imagerie */}
+      <ProjectTemplate 
+        thumbnailURL={process.env.PUBLIC_URL + '/images/desktop_album_view.jpg'}
+        projectURL='https://imagerie-sei.herokuapp.com/'
+        githubURL='https://git.generalassemb.ly/judykim-ga/project-one'
+        left='-40%'
+      />
+
+      {/* Project 3 - Tutor Marketplace */}
+      <ProjectTemplate
+        thumbnailURL={process.env.PUBLIC_URL + '/images/tutor_marketplace_preview.png'}
+        projectURL='https://tutor-marketplace-sei.herokuapp.com/'
+        githubURL='https://git.generalassemb.ly/judykim-ga/TutorMarketplace'
+        left='-10%'
+      />
+
+      {/* Project 4 - Avocados Anonymous */}
+      <ProjectTemplate
+        thumbnailURL={process.env.PUBLIC_URL + '/images/home-page.png'}
+        projectURL='https://avo-anon-collabathon.herokuapp.com/'
+        githubURL='https://git.generalassemb.ly/judykim-ga/collabathon'
+        left='-40%'
+      />
+
+      {/* Project 5 - Sandpiper */}
+      <ProjectTemplate
+        thumbnailURL={process.env.PUBLIC_URL + '/images/desktop-profile.png'}
+        projectURL='https://sandpiper-react.herokuapp.com/'
+        githubURL='https://git.generalassemb.ly/judykim-ga/final-project-client'
+        left='-30%'
+      />
+
+      {/* Project 6 - Portfolio */}
+      <ProjectTemplate
+      />
     </div>
   )
 }
--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProjectTemplate = (props) => {
+  const info = useInfoDisplay();
+  // position thumbnail within thumbnail frame
+  const [leftPosition, setLeftPostition] = useState({left: props.left});
+
   return (
     <>
-      <h3>project technology</h3>
-      <h3>best parts of project</h3>
-      <h3>what I'm most proud of</h3>
-      this will be filled with stuff
+    <div className="row">
+      <div className="col l4">
+        <div className="project-thumbnail-frame">
+          <a href={props.projectURL} target="_blank" rel="noopener noreferrer">
+            <img className="project-thumbnail" 
+            style={leftPosition}
+            src={props.thumbnailURL} alt="project thumbnail" />
+          </a>
+        </div>
+      </div>
+      <div className="col l8">
+        this will be filled with stuff
+
+        {info.infoDisplay.display === 'none' ?
+          <div className="clickable-icon wide" onClick={info.toggleInfoDisplay}>
+            {/* Show More */}
+            see more <i className="fa fa-chevron-right fa-lg" aria-hidden="true" ></i>
+          </div>
+          :
+          <div className="clickable-icon wide" onClick={info.toggleInfoDisplay}>
+            {/* Hide */}
+            see less <i className="fa fa-chevron-down fa-lg" aria-hidden="true" ></i>
+          </div>
+        }
+        <div style={info.infoDisplay} className="project-info">
+          more info
+          <h3>project technology</h3>
+          <h3>best parts of project</h3>
+          <h3>what I'm most proud of</h3>
+          <div className="clickable-icon wide">
+            <a href={props.githubURL}>
+            <i className="fa fa-github fa-lg" aria-label="link to github repo"></i> Github Repo
+            </a>
+        </div>
+      </div>
+    </div>
+    </div>
     </>
   )
 }
 
 export default ProjectTemplate;
+
+// custom hook to toggle element display between flex and none
+export const useInfoDisplay = () => {
+  const [infoDisplay, setInfoDisplay] = useState({display: 'none'})
+  const toggleInfoDisplay = () => {
+    // toggle show info state
+    if (infoDisplay.display === 'none') {
+      setInfoDisplay({display: 'flex'})
+    } else {
+      setInfoDisplay({display: 'none'})
+    }
+  }
+  return ({
+    infoDisplay,
+    toggleInfoDisplay
+  })
+}
