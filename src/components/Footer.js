776c98473f86eab2a88e26aeb7816715040278d2
Judy <kimjudyh@gmail.com>
2020-06-27 01:42:38 -0700

    removed hamburger menu from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,6 @@ const Footer = () => {
       <div className="container">
           <a className="nav-link" href="#about-link">GitHub</a>
           <a className="nav-link" href="#project-link">LinkedIn</a>
-          {/* Hamburger Menu Button */}
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#footerDropdown" aria-controls="footerDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      {/* <span className="navbar-toggler-icon"></span> */}
-      <i className="fa fa-bars"></i>
-    </button>
-    {/* Links in dropdown */}
-    <div className="collapse navbar-collapse ml-auto" id="footerDropdown">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item active" data-toggle="collapse" data-target="#footerDropdown">
-        </li>
-      </ul>
-      </div>
       </div>
     </footer>
 
