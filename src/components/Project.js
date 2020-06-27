c57435a66dc8b2ed03076dbb5a237948a713a22d
Judy <kimjudyh@gmail.com>
2020-06-27 01:42:16 -0700

    added info for individual projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectTemplate from './ProjectTemplate';
+import { project1, project2, project3, project4, project5, project6 } from './ProjectWriteup';
 
 const Project = () => {
   return (
@@ -13,6 +14,9 @@ const Project = () => {
         thumbnailURL={process.env.PUBLIC_URL + '/images/game_preview.png'}
         projectURL='https://pages.git.generalassemb.ly/judykim-ga/pet-a-bunny/'
         githubURL='https://git.generalassemb.ly/judykim-ga/pet-a-bunny'
+        summary={project1.summary}
+        projectTechnology={project1.projectTechnology}
+        timeline={project1.timeline}
       />
 
       {/* Project 2 - Imagerie */}
@@ -20,6 +24,9 @@ const Project = () => {
         thumbnailURL={process.env.PUBLIC_URL + '/images/desktop_album_view.jpg'}
         projectURL='https://imagerie-sei.herokuapp.com/'
         githubURL='https://git.generalassemb.ly/judykim-ga/project-one'
+        summary={project2.summary}
+        projectTechnology={project2.projectTechnology}
+        timeline={project2.timeline}
         left='-40%'
       />
 
@@ -28,6 +35,9 @@ const Project = () => {
         thumbnailURL={process.env.PUBLIC_URL + '/images/tutor_marketplace_preview.png'}
         projectURL='https://tutor-marketplace-sei.herokuapp.com/'
         githubURL='https://git.generalassemb.ly/judykim-ga/TutorMarketplace'
+        summary={project3.summary}
+        projectTechnology={project3.projectTechnology}
+        timeline={project3.timeline}
         left='-10%'
       />
 
@@ -36,6 +46,9 @@ const Project = () => {
         thumbnailURL={process.env.PUBLIC_URL + '/images/home-page.png'}
         projectURL='https://avo-anon-collabathon.herokuapp.com/'
         githubURL='https://git.generalassemb.ly/judykim-ga/collabathon'
+        summary={project4.summary}
+        projectTechnology={project4.projectTechnology}
+        timeline={project4.timeline}
         left='-40%'
       />
 
@@ -45,10 +58,23 @@ const Project = () => {
         projectURL='https://sandpiper-react.herokuapp.com/'
         githubURL='https://git.generalassemb.ly/judykim-ga/final-project-client'
         figmaURL='https://www.figma.com/file/aC2C1JFg0NX5luGDARJL6x/birdwatching-journal?node-id=0%3A1'
+        summary={project5.summary}
+        projectTechnology={project5.projectTechnology}
+        timeline={project5.timeline}
         left='-30%'
       />
 
       {/* Project 6 - Portfolio */}
+      <ProjectTemplate 
+        thumbnailURL={process.env.PUBLIC_URL + '/images/anthro_shirt.png'}
+        projectURL='https://www.anthropologie.com/shop/farm-rio-helena-clip-dot-blouse?category=new-clothes&color=090&type=STANDARD&quantity=1'
+        githubURL='https://git.generalassemb.ly/judykim-ga/portfolio'
+        figmaURL='#'
+        summary={project6.summary}
+        projectTechnology={project6.projectTechnology}
+        timeline={project6.timeline}
+        left='8%'
+      />
     </div>
   )
 }
---
+++ src/components/ProjectWriteup.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export const project1 = {
+  summary: 
+  <p>
+    Pet-a-Bunny was my first project at General Assembly, and my first project using Javascript! My favorite part of this project was implementing achievements and having banners pop up when you obtain an achievement. Making this game gave me some serious nostalgia for my Neopets days. 
+    The most challenging part of this project was dealing with timers.
+
+  </p>,
+  projectTechnology:
+  <p>vanilla javascript, jQuery</p>,
+  timeline:
+  <p>April 3 - April 10, 2020</p>
+}
+
+export const project2 = {
+  summary:
+  <p>
+    Imagerie was a partner project and my first foray into a MEN-stack website.
+  </p>,
+  projectTechnology:
+  <p>Tech Stack: Node.js, Express, MongoDB</p>,
+  timeline:
+  <p>April 23 - May 1, 2020</p>
+}
+
+export const project3 = {
+  summary:
+  <p>
+    Tutor Marketplace, a clone of tutoring websites
+  </p>,
+  projectTechnology:
+  <>
+  <ul>
+    <li>Django</li>
+    <li>Python</li>
+    <li>PostgreSQL</li>
+  </ul>
+  </>,
+  timeline:
+  <p>May 13 - May 20, 2020</p>
+}
+
+export const project4 = {
+  summary:
+  <p>
+    Collabathon!
+  </p>,
+  projectTechnology:
+  <p>Tech Stack: Node.js, Express, MongoDB</p>,
+  timeline:
+  <p>May 21 - May 22, 2020</p>
+}
+
+export const project5 = {
+  summary:
+  <p>
+    Sandpiper was my Final project at General Assembly! birdwatching hoorayyyy
+  </p>,
+  projectTechnology:
+  <p>Tech Stack: React, Node.js, Express, MongoDB</p>,
+  timeline:
+  <p>June 4 - June 15, 2020</p>
+}
+
+export const project6 = {
+  summary:
+  <p>
+    this portfolio website. Is talking about it kind of like breaking the fourth wall? Shout out to this Anthropologie shirt for inspiring the color scheme of this portfolio #notsponsored.
+  </p>,
+  projectTechnology:
+  <p>React</p>,
+  timeline:
+  <p>June 25 - June 29, 2020</p>
+}
+
+const ProjectWriteup = {
+  project1
+}
+
+export default ProjectWriteup;
