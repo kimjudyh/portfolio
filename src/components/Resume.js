43eadd261d27f56bcdcb9c0a91825cbb10574b64
Judy <kimjudyh@gmail.com>
2020-06-26 20:32:31 -0700

    added pdf of resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Resume = () => {
+  const [resumeURL, setResumeURL] = useState( process.env.PUBLIC_URL + '/assets/JudyKim_portfolio_fullstack.pdf')
   return (
-    <div className="resume">
+    <div className="resume" id="resume-link">
       <h2>resume</h2>
+      owo
+      <a href={resumeURL}>View and download my resume</a>
+      <div className="pdf">
+      </div>
     </div>
   )
 }
