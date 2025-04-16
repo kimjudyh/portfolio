e192d6a97705a00e2cff1377bbcc78ae78efe29a
kimjudyh <kimjudyh@gmail.com>
2025-04-16 14:23:37 -0700

    adds where i used to work, rewords the other stuff

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,24 @@ const About = () => {
       <h2>about me</h2>
       <div className="about-me">
         <p>
-         Hi, I’m Judy! I recently finished a 3 month coding bootcamp at General Assembly. I currently reside in the Bay Area. Previously, I was a mechanical design and manufacturing engineer at two spacecraft companies. I went to Cornell University and am originally from Northern Virginia.
+         Hi, I’m Judy! I'm a software developer who is currently based in Mountain View. I previously worked at First Republic
+         Bank, which was then acquired by JPMorgan Chase.
+         I finished a 3 month coding bootcamp at General Assembly in order to transition into software development. 
+         Previously, I was a mechanical design and manufacturing engineer at two spacecraft 
+         companies. I went to Cornell University and am originally from Northern Virginia.
          </p>
          <p>
-          I love birds and watching them do cute, amusing, and inexplicable bird things. A birdfeeder that I set up on my porch before the lockdown started has provided hours of entertainment. My most intense birdwatching experience was when I went on an 8-hour boat ride to the Farallon Islands. I stood the whole time so I didn’t get seasick and was rewarded by spotting a black-footed albatross! 
+          I love birdwatching, both locally at Shoreline and abroad in places like Korea and India.
+          My most intense birdwatching experience was when I went on an 8-hour boat ride to the Farallon Islands. 
+          I stood the whole time so I didn’t get seasick and saw a black-footed albatross! 
         </p>
         <p>
-          Other hobbies I enjoy are cooking (especially Korean food), gardening, and skiing. For gardening, I mainly propagate succulents and African Violets until there's no more room by the windows for yet another pot of fuzzy green leaves. I love skiing and the awesome places I get to see, like Blackcomb Glacier and Lake Tahoe. 
+          Other hobbies I enjoy are cooking (especially Korean food), gardening, and skiing. 
+          I have a small plot in a nearby community garden and grow tomatoes, arugula, and raspberries, among 
+          other things.
+          I like to do day trips up to Tahoe and look forward to my annual group trips with my husband and his friends.
+          We've gone to amazing places like Whistler, Snowbird, Aspen, and Telluride.
+
         </p>
       </div>
       <div className="about-picture-container">
